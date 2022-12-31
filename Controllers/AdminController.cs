421508b3c06268c0bc4ff77eda42e1d2a6ec3e63
Azmi Çağrı Karakaya <azmi.karakaya@ogr.sakarya.edu.tr>
2022-12-31 22:17:04 +0300

    Admin panelinin layout kısımlarını ve ürün ekleme işlemlerini yaptık.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ozmutluweb.Models.Siniflar;
+
+namespace ozmutluweb.Controllers
+{
+    public class AdminController : Controller
+    {
+        Context c = new Context();
+        public IActionResult Index()
+        {
+            var degerler = c.Urunlers.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public IActionResult YeniUrun() //Bunlardan bir tanesi httpgette çalışağı için aynı metottan iki tane oluşturuyoruz.
+        {
+            return View(); //Sayfa yüklenince hiçbir şey yapma sayfanın bos halini döndür
+        }
+
+        [HttpPost]
+        public IActionResult YeniUrun(Urunler u)
+        {
+            c.Urunlers.Add(u);
+            c.SaveChanges(); //C# executenonquery mantığı
+            return RedirectToAction("Index"); //Sayfada bir şey gönderdiğim zaman buradaki işlemleri döndür.
+        }
+    }
+}
--- Controllers/OzmutluController.cs
+++ Controllers/OzmutluController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ozmutluweb.Models.Siniflar;
 
 namespace ozmutluweb.Controllers
 {
     public class OzmutluController : Controller
     {
+        Context c = new Context();
         public IActionResult Index()
+        {
+            var degerler = c.Urunlers.ToList();
+            return View(degerler);
+        }
+
+        public IActionResult About()
         {
             return View();
         }
